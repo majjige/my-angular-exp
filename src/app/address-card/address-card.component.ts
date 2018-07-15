b895cd9418af7af3340627ed6587ee47e9ac3a2a
majjige <santhavasantha@gmail.com>
2018-07-15 22:52:02 +0530

    life cycle hooks ngoninit

--- src/app/address-card/address-card.component.ts
+++ src/app/address-card/address-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-address-card',
@@ -7,16 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddressCardComponent implements OnInit {
 
-  user = {
-    name: 'Foo Bar',
-    title: 'Software Developer',
-    address: '1234, Main Stm City, State, 100000',
-    phone: ['123-123-1234', '456-456-4567']
-  };
+  user: any;
+  // tslint:disable-next-line:no-input-rename
+  @Input('name') userName: string;
+  @Input() title: string;
 
   constructor() { }
 
   ngOnInit() {
+    this.user = {
+      name: this.userName,
+      title: this.title,
+      address: '1234, Main Stm City, State, 100000',
+      phone: ['123-123-1234', '456-456-4567']
+    };
   }
 
 }
