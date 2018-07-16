4d125795f96d2f6921bd98f0c4340fcd30a61d13
majjige <santhavasantha@gmail.com>
2018-07-16 11:50:26 +0530

    two way binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PrintMyNameComponent } from './print-my-name/print-my-name.component';
 import { DateComponent } from './date/date.component';
 import { AddressCardComponent } from './address-card/address-card.component';
+import { TextboxComponent } from './textbox/textbox.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PrintMyNameComponent,
     DateComponent,
-    AddressCardComponent
+    AddressCardComponent,
+    TextboxComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/textbox/textbox.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-textbox',
+  templateUrl: './textbox.component.html',
+  styleUrls: ['./textbox.component.css']
+})
+export class TextboxComponent implements OnInit {
+
+  inputText = 'some text';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
